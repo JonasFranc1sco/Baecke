63ff385992afa108cefb0b369c2e8d8dc656683c
JonasFranc1sco <jonasgouveia19@gmail.com>
2025-08-28 21:29:10 -0400

    Models e Forms do questionário inciados

--- questionnarie/models.py
+++ questionnarie/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Profile (models.Model):
+    name = models.CharField(max_length=300),
+    birthday = models.DateField(),
+    height = models.FloatField(max_length=3),
+    weight = models.FloatField(max_length=400)
+
+class Question (models.Model):
+    # Models Choices
+    QUESTION_CATEGORY_CHOICES = {
+        "AFO": "Atividades físicas ocupacionais",
+        "EFL": "Exercícios físicos no lazer",
+        "ALL": "Atividades físicas de lazer e locomoção",
+    }
+    name = models.CharField(max_length=500)
+    category = models.CharField(max_length=3, choices=QUESTION_CATEGORY_CHOICES)
+    
+class Choice (models.Model):
+    name = models.CharField(max_length=300)
+    question = models.models.ForeignKey(Question, on_delete=models.CASCADE)
+    value = models.DecimalField(max_length=5)
--- questionnarie/views.py
+++ questionnarie/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from forms import ProfileForm, QuestionForm, ChoiceForm
 # Create your views here.
