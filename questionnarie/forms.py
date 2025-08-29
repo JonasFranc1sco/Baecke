10125e9fad1d4d120cfa4f3e505802512b766daf
Jonas <jhoucdn@gmail.com>
2025-08-28 21:29:10 -0400

    Models e Forms do questionário inciados

---
+++ questionnarie/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from models import Profile, Question, Choice
+
+class ProfileForm(forms.Form):
+    class Meta:
+        model = Profile
+        fields = ['name', 'birthday', 'height', 'weight']
+
+class QuestionForm(forms.Form):
+    class Meta:
+        model = Question
+        fields = ['name', 'category']
+        
+class ChoiceForm(forms.Form):
+    class Meta:
+        model = Choice
+        fields = ['name', 'question', 'value']
